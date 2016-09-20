a96cd1586b0434f19fb10bb7f0fd4edd448f79df
3pehrdev <sepehrsalehi0111@gmail.com>
2016-09-20 16:33:19 +0430

    Update test.lua

--- plugins/test.lua
+++ plugins/test.lua
@@ -3,11 +3,12 @@ local rw1_texts = {'teSt version'}
 local rows ={kmakerow(rw1_texts)}
 return kmake(rows)
 end
+local action = function(msg, blocks, ln)
 if blocks[1] == 'ver' then
 local text_start = "*ver 1.2*"
 api.sendMessage(msg.chat.id,text_start, true, true, nil, true, make_menu())
 end
-
+   end
 
 return {
 	action = action,
