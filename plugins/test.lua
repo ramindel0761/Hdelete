8b35bc048fc0c584b6450353781ca8cc104b4419
3pehrdev <sepehrsalehi0111@gmail.com>
2016-09-20 16:21:24 +0430

    Create test.lua

---
+++ plugins/test.lua
@@ -0,0 +1,20 @@
+local function make_menu()
+local rw1_texts = {'teSt version'}
+local rows ={kmakerow(rw1_texts)}
+return kmake(rows)
+end
+if blocks[1] == 'ver' then
+local text_start = "*ver 1.2*"
+api.sendMessage(msg.chat.id,text_start, true, true, nil, true, make_menu())
+end
+
+    end
+
+end
+
+return {
+	action = action,
+	triggers = {
+	    '^/(ver)$',
+    }
+}
