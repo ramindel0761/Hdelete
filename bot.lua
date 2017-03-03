786b1a1907536af7e218528dc879e9468c17b590
3pehrdev <sepehrsalehi0111@gmail.com>
2017-03-03 11:44:18 +0330

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -3137,15 +3137,15 @@ local pin_id = database:get('pinnedmsg'..msg.chat_id_)
    end
     end
 	end
- if is_mod(msg.sender_user_id_, msg.chat_id_) then
-         send(msg.chat_id_, msg.message_id_, 1, '*You Are My Admin!!!!!*\n*I Cant Show You What Edit :DDD*', 1, 'md')
-    else
 	if database:get('editmsg'..msg.chat_id_) == 'delmsg' then
         local id = msg.message_id_
         local msgs = {[0] = id}
         local chat = msg.chat_id_
               delete_msg(chat,msgs)
 	elseif database:get('editmsg'..msg.chat_id_) == 'didam' then
+ if is_mod(msg.sender_user_id_, msg.chat_id_) then
+         send(msg.chat_id_, msg.message_id_, 1, '*You Are My Admin!!!!!*\n*I Cant Show You What Edit :DDD*', 1, 'md')
+    else
 	if database:get('bot:editid'..msg.message_id_) then
 		local old_text = database:get('bot:editid'..msg.message_id_)
 	    send(msg.chat_id_, msg.message_id_, 1, 'Dont Edit!\n*I See You What Say :D*:\n*Edit Was Locked!*\n*I"ll Delete Your Message*\n_Your Say: _*'..old_text..'*', 1, 'md')				
