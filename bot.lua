f3a7bbeedd10f281e3d3140f635275bac7331e13
3pehrdev <sepehrsalehi0111@gmail.com>
2017-03-03 11:35:43 +0330

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -3145,9 +3145,13 @@ local pin_id = database:get('pinnedmsg'..msg.chat_id_)
 	elseif database:get('editmsg'..msg.chat_id_) == 'didam' then
 	if database:get('bot:editid'..msg.message_id_) then
 		local old_text = database:get('bot:editid'..msg.message_id_)
+	if is_mod(result.sender_user_id_, result.chat_id_) then
+         send(msg.chat_id_, msg.id_, 1, '*You Are My Admin!!!!!*\n*I Cant Show You What Edit :DDD*', 1, 'md')
+    else
 	    send(msg.chat_id_, msg.message_id_, 1, 'Dont Edit!\n*I See You What Say :D*:\n*Edit Was Locked!*\n*I"ll Delete Your Message*\n_Your Say: _*'..old_text..'*', 1, 'md')				
 	end
 	end
+			end
     getMessage(msg.chat_id_, msg.message_id_,get_msg_contact)
   -----------------------------------------------------------------------------------------------
   elseif (data.ID == "UpdateOption" and data.name_ == "my_id") then
